2dac603ed71dc5584e470e6fb7f07da784847d16
Cesare Tonola <c.tonola001@unibs.it>
2024-07-02 17:20:49 +0200

    first commit

---
+++ include/moveit_trajectory_processor/moveit_trajectory_processor.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <trajectories_processors_lib/spline_trajectory_processor.h>
+
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+#include <moveit/trajectory_processing/iterative_spline_parameterization.h>
+#include <moveit/trajectory_processing/time_optimal_trajectory_generation.h>
+
+/**
+ * @file MoveitTrajectoryProcessor.h
+ * @brief Contains the declaration of the MoveitTrajectoryProcessor class.
+ */
+
+namespace trajectories_processors
+{
+class MoveitTrajectoryProcessor;
+typedef std::shared_ptr<MoveitTrajectoryProcessor> MoveitTrajectoryProcessorPtr;
+
+/**
+ * @brief The MoveitTrajectoryProcessor class processes trajectories using Moveit algorithms and interpolates with splines of different orders.
+ */
+
+class MoveitTrajectoryProcessor: public SplineTrajectoryProcessor
+{
+protected:
+  /**
+   * @brief Enum for selecting the moveit algorithm for trajectory planning.
+   *        - ITP: iterative time parametrization
+   *        - ISP: iterative spline parametrization
+   *        - TOTG: time optimal trajectory generation
+   */
+  enum class moveit_alg_t {ITP, ISP, TOTG};
+
+  /**
+   * @brief The selected moveit algorithm, default: ISP.
+   */
+  moveit_alg_t moveit_alg_ = moveit_alg_t::ISP;
+
+  /**
+   * @brief group_name_ The name of the robot group.
+   */
+  std::string group_name_;
+
+  /**
+   * @brief robot_model_ pointer to the Moveit RobotModel.
+   */
+  moveit::core::RobotModelPtr robot_model_;
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  /**
+   * @brief Constructors.
+   */
+  MoveitTrajectoryProcessor(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model):
+    SplineTrajectoryProcessor(constraints,param_ns,logger),group_name_(group_name),robot_model_(robot_model){}
+  MoveitTrajectoryProcessor(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,path),group_name_(group_name),robot_model_(robot_model){}
+  MoveitTrajectoryProcessor(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const spline_order_t& spline_order, const moveit_alg_t& moveit_alg, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,spline_order),moveit_alg_(moveit_alg),group_name_(group_name),robot_model_(robot_model){}
+  MoveitTrajectoryProcessor(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path,const spline_order_t& spline_order,const moveit_alg_t& moveit_alg, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model):
+    SplineTrajectoryProcessor(constraints,param_ns,logger,path,spline_order),moveit_alg_(moveit_alg),group_name_(group_name),robot_model_(robot_model){}
+
+  /**
+   * @brief init Initializes the TrajectoryProcessor object. This function should be called when the void constructor is called and it is used mainly for plugins.
+   * @param constraints The kinodynamics constraints of the robot to be considered for trajectory generation.
+   * @param param_ns_ The namespace under which to read the parameters with cnr_param.
+   * @param logger The logger for logging purposes.
+   * @param path The path for which the time-law need to be computed.
+   * @param group_name The robot group name.
+   * @param robot_model A Moveit RobotModel pointer to the robot model.
+   * @return true if successfull
+   */
+  virtual bool init(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model);
+  virtual bool init(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path, const std::string& group_name, const moveit::core::RobotModelPtr& robot_model);
+
+  /**
+   * @brief Sets the moveit algorithm and clears the trajectory.
+   * @param moveit_alg The moveit algorithm to set.
+   */
+  void setMoveitAlg(const moveit_alg_t& moveit_alg)
+  {
+    moveit_alg_ = moveit_alg;
+    trj_.clear();
+  }
+
+  /**
+   * @brief Function to compute the trajectory.
+   * @param initial_state The initial robot state.
+   * @param final_state The final robot state.
+   * @return True if the trajectory computation is successful, false otherwise.
+   */
+  virtual bool computeTrj() override;
+  virtual bool computeTrj(const RobotState& initial_state) override;
+  virtual bool computeTrj(const RobotState& initial_state, const RobotState& final_state) override;
+};
+}
---
+++ src/moveit_trajectory_processor.cpp
@@ -0,0 +1,204 @@
+#include <moveit_trajectory_processor/moveit_trajectory_processor.h>
+
+namespace trajectories_processors
+{
+
+void fromEigen2Vector(const Eigen::VectorXd& eigen, std::vector<double> vector)
+{
+  vector.clear();
+  vector.resize(eigen.rows());
+  Eigen::VectorXd::Map(&vector[0], eigen.rows()) = eigen;
+}
+
+moveit::core::RobotState fromWaypoint2State(const Eigen::VectorXd& waypoint,
+                                            const moveit::core::RobotStateConstPtr& robot_state,
+                                            const std::string& group_name)
+{
+  moveit::core::RobotStatePtr state = std::make_shared<moveit::core::RobotState>(robot_state);
+  state->setJointGroupPositions(group_name,waypoint);
+  state->update();
+
+  return state;
+}
+
+bool fromWaypoints2States(const std::vector<Eigen::VectorXd>& waypoints,
+                          const moveit::core::RobotStateConstPtr& robot_state,
+                          const std::string& group_name,
+                          std::vector<moveit::core::RobotState>& states)
+{
+  states.clear();
+  states.reserve(waypoints.size());
+
+  for(const auto& waypoint: waypoints)
+  {
+    auto state = fromWaypoint2State(waypoint,robot_state,group_name);
+    states.push_back(state);
+  }
+
+  return true;
+}
+
+bool MoveitTrajectoryProcessor::computeTrj()
+{
+  RobotState initial_state;
+  return computeTrj(initial_state);
+}
+
+bool MoveitTrajectoryProcessor::computeTrj(const RobotState& initial_state)
+{
+  RobotState final_state;
+  return computeTrj(initial_state,final_state);
+}
+
+bool MoveitTrajectoryProcessor::computeTrj(const RobotState& initial_state, const RobotState& final_state)
+{
+  if(path_.empty())
+  {
+    CNR_ERROR(logger_,"Path not defined, cannot compute time law");
+    return false;
+  }
+
+  if(path_.size() == 1)
+  {
+    CNR_ERROR(logger_,"Path is a single waypoint defined, cannot compute time law");
+    return false;
+  }
+
+  robot_state::RobotStatePtr robot_state = std::make_shared<robot_state::RobotState>(robot_model_);
+
+  std::vector<moveit::core::RobotState> states_vector;
+  if(not fromWaypoints2States(path_,robot_state,group_name_,states_vector))
+    return false;
+
+  robot_trajectory::RobotTrajectoryPtr trj = std::make_shared<robot_trajectory::RobotTrajectory>(robot_model_);
+
+  for(unsigned int j=0; j<path_.size();j++)
+  {
+    if(j==0 && (not initial_state.pos_.empty()))
+    {
+      states_vector.at(j).setJointGroupPositions    (group_name_,initial_state.pos_);
+      states_vector.at(j).setJointGroupVelocities   (group_name_,initial_state.vel_);
+      states_vector.at(j).setJointGroupAccelerations(group_name_,initial_state.acc_);
+    }
+    else if(j==(path_.size()-1) && (not final_state.pos_.empty()))
+    {
+      states_vector.at(j).setJointGroupPositions    (group_name_,final_state.pos_);
+      states_vector.at(j).setJointGroupVelocities   (group_name_,final_state.vel_);
+      states_vector.at(j).setJointGroupAccelerations(group_name_,final_state.acc_);
+    }
+    trj->addSuffixWayPoint(states_vector.at(j),0.001);
+  }
+
+  //Time parametrization
+  trajectory_processing::TimeParameterizationPtr tp;
+  if(moveit_alg_ == moveit_alg_t::ITP)
+    tp = std::make_shared<trajectory_processing::IterativeParabolicTimeParameterization>();
+  else if(moveit_alg_ == moveit_alg_t::ISP)
+    tp = std::make_shared<trajectory_processing::IterativeSplineParameterization>();
+  else if(moveit_alg_ == moveit_alg_t::TOTG)
+    tp = std::make_shared<trajectory_processing::TimeOptimalTrajectoryGeneration>();
+
+  tp->computeTimeStamps(*trj);
+
+  TrjPoint pt;
+  moveit_msgs::RobotTrajectory trj_msg;
+  trj->getRobotTrajectoryMsg(trj_msg);
+
+  trj_.clear();
+  for(const auto& trj_point:trj_msg.joint_trajectory.points)
+  {
+    pt.state_.pos_ = std::move(trj_point.positions);
+    pt.state_.vel_ = std::move(trj_point.velocities);
+    pt.state_.acc_ = std::move(trj_point.accelerations);
+    pt.state_.eff_ = std::move(trj_point.effort);
+    pt.time_from_start_ = std::move(trj_point.time_from_start);
+
+    trj_.push_back(pt);
+  }
+
+  return true;
+}
+
+bool MoveitTrajectoryProcessor::init(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::string &group_name, const moveit::core::RobotModelPtr &robot_model)
+{
+  SplineTrajectoryProcessor::init(constraints,param_ns,logger);
+
+  group_name_ = group_name;
+  robot_model_ = robot_model;
+
+  std::string moveit_alg;
+  std::string what, full_param_name = param_ns+"/moveit_alg";
+  if(cnr::param::has(full_param_name, what))
+  {
+    if(not cnr::param::get(full_param_name, moveit_alg, what))
+    {
+      CNR_ERROR(logger, "Cannot load " << full_param_name + " parameter.\n"<<what);
+      return false;
+    }
+    else
+    {
+      std::transform(moveit_alg.begin(), moveit_alg.end(), moveit_alg.begin(), ::toupper);
+
+      if(moveit_alg == std::string("ITP"))
+        moveit_alg_ = moveit_alg_t::ITP;
+      else if(moveit_alg == std::string("ISP"))
+        moveit_alg_ = moveit_alg_t::ISP;
+      else if(moveit_alg == std::string("TOTG"))
+        moveit_alg_ = moveit_alg_t::TOTG;
+      else
+      {
+        CNR_ERROR(logger_,moveit_alg<<" algorithm not available");
+        return false;
+      }
+    }
+  }
+  else
+  {
+    CNR_WARN(logger, full_param_name + " parameter not available.\n"<<what);
+    return false;
+  }
+
+  return true;
+}
+bool MoveitTrajectoryProcessor::init(const KinodynamicConstraints& constraints, const std::string& param_ns, const cnr_logger::TraceLoggerPtr& logger, const std::vector<Eigen::VectorXd>& path, const std::string &group_name, const moveit::core::RobotModelPtr &robot_model)
+{
+  SplineTrajectoryProcessor::init(constraints,param_ns,logger,path);
+
+  group_name_ = group_name;
+  robot_model_ = robot_model;
+
+  std::string moveit_alg;
+  std::string what, full_param_name = param_ns+"/moveit_alg";
+  if(cnr::param::has(full_param_name, what))
+  {
+    if(not cnr::param::get(full_param_name, moveit_alg, what))
+    {
+      CNR_ERROR(logger, "Cannot load " << full_param_name + " parameter.\n"<<what);
+      return false;
+    }
+    else
+    {
+      std::transform(moveit_alg.begin(), moveit_alg.end(), moveit_alg.begin(), ::toupper);
+
+      if(moveit_alg == std::string("ITP"))
+        moveit_alg_ = moveit_alg_t::ITP;
+      else if(moveit_alg == std::string("ISP"))
+        moveit_alg_ = moveit_alg_t::ISP;
+      else if(moveit_alg == std::string("TOTG"))
+        moveit_alg_ = moveit_alg_t::TOTG;
+      else
+      {
+        CNR_ERROR(logger_,moveit_alg<<" algorithm not available");
+        return false;
+      }
+    }
+  }
+  else
+  {
+    CNR_WARN(logger, full_param_name + " parameter not available.\n"<<what);
+    return false;
+  }
+
+  return true;
+}
+}
